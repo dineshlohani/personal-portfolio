7f15c8e6e3ede80096265c898816bbcd303731d2
DineshLohani <dineshlohani333@gmail.com>
2025-01-24 15:18:59 +0545

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Dinesh Lohani</h3>
+          Designed and Developed by Dinesh Lohani
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} D͜͡ʟ</h3>
